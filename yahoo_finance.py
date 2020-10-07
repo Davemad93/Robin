d22b63f48e93a3581586b5597334ce95b4dce1f9
Davemad93 <Davemad93@gmail.com>
2020-10-07 12:43:49 -0500

    Reduce 60 days to 59. Was getting error randomly that 5min date range was not within 60 days.

--- yahoo_finance.py
+++ yahoo_finance.py
@@ -7,8 +7,8 @@ def get_stock_history(stock_name, interval):
     stock = yf.Ticker(stock_name)
 
     today = date.datetime.today()
-    sixty_days_before = (today-timedelta(days=60))
+    sixty_days_before = (today-timedelta(days=59))
 
     hist = stock.history(interval=interval, start=sixty_days_before, end=today, prepost="True")
 
-    return hist["Close"][:-1]
+    return hist["Close"]
