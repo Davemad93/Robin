3d38aa5a9c778493193cbf0e130e5f2c08348630
Davemad93 <Davemad93@gmail.com>
2020-10-07 12:56:54 -0500

    Formatted day trades and transaction tracking in a more readable fashion for database.txt. Updated some depreciated functions in testing program.

--- database_function_test.py
+++ database_function_test.py
@@ -7,13 +7,13 @@ database = "database.txt"
 class TestSum(unittest.TestCase):
     def test_get_number_of_trades_from_database(self):
         number_of_trades = database_function.get_number_of_trades(database)
-        self.assertEquals(3, int(number_of_trades))
+        self.assertEqual(3, int(number_of_trades))
     
     def test_update_number_of_trades_in_database(self):
         trade_number = 1
         database_function.update_trade_number(str(trade_number), database)
         number_of_trades = database_function.get_number_of_trades(database)
-        self.assertEquals(1, int(number_of_trades))
+        self.assertEqual(1, int(number_of_trades))
 
 if __name__ == '__main__':
     unittest.main()
